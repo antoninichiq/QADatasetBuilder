f7339d320d48fdceee2909bb287c96684ccec5d8
antoninichiq <142185935+antoninichiq@users.noreply.github.com>
2024-03-08 18:12:40 -0300

    Add files via upload

---
+++ QADatasetBuilder/Using_the_classes.py
@@ -0,0 +1,26 @@
+from CreateDataset import ChunkFile, CreateDataset
+
+
+    # Insert pdfs or wikipedia pages here
+files = ["https://en.wikipedia.org/wiki/Thermoplastic",
+                           "path/to/your/document.pdf",               
+        ]
+
+
+scraped_files = "all_data.txt"
+
+for i in files:
+    file_reader = ChunkFile(
+        remove_introduction=True, 
+        remove_references=True,
+        file_path=i, 
+        file_name=scraped_files)
+    file_reader.initiate_processing()
+
+dataset = CreateDataset(
+    file_path= scraped_files,
+    file_name = "QAFile.json",
+    google_api_key=google_api_key
+    )
+
+dataset.create_dataset()
